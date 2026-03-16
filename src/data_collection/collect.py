630e9b3144a365e0d8d6361060806779920d64e3
Justin <juswerth@gmx.de>
2026-03-16 17:19:48 +0100

    Print out config variable for testing.

--- src/data_collection/collect.py
+++ src/data_collection/collect.py
@@ -1,11 +1,13 @@
 from time import sleep
-
+from os import environ
 
 # Mock for data collection function
 def collect_data():
     print("Service up.")
     while True:
-        print("Data Collection Mock.", flush=True)
+        print("Data Collection Mock.")
+        print(f"Variable test: {(t := environ.get('DC__DEV__TEST_VAR'))} ({bool(t)})")
+        print(end="", flush=True)
         sleep(5)
 
 
