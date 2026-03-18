846a929ca564ac824dbdf4bb686544d1cf12dbe3
Justin <juswerth@gmx.de>
2026-03-18 16:16:46 +0100

    Removed reddit API secret

--- src/data_collection/collect.py
+++ src/data_collection/collect.py
@@ -1,12 +1,5 @@
 from time import sleep
 from os import environ
-from functools import cache
-
-# Read the Reddit API secret
-@cache
-def get_api_secret():
-    with open("/run/secrets/reddit_api_secret", "r") as f:
-        return f.read().strip()
 
 
 # Mock for data collection function
@@ -15,7 +8,6 @@ def collect_data():
     while True:
         print("Data Collection Mock.")
         print(f"Variable test: {(t := environ.get('DC__DEV__TEST_VAR'))} ({bool(t)})")
-        print(f"Secret: {bool(get_api_secret())}, len={len(get_api_secret())}")
         print(end="", flush=True)
         sleep(5)
 
