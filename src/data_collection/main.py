f2bdcec5064e7ebe0b3756eb598c8c8a5953974a
Justin <juswerth@gmx.de>
2026-03-23 19:43:27 +0100

    Update main loop for data retrieval

--- src/data_collection/main.py
+++ src/data_collection/main.py
@@ -3,7 +3,9 @@ import constants as c
 import networking
 
 # Python module imports
+import json
 from time import sleep
+from datetime import datetime
 
 
 # Print the system status variables
@@ -18,8 +20,27 @@ def collect_data():
     print_status()
 
     while True:
+        print("Fetching data...", flush=True)
+        fetch_time = datetime.now()
         res = networking.fetch_all_stops()
-        print(res)
+
+        # Save results
+        res_dict = dict(
+            fetched_data=[
+                {
+                    "stop": stop["stop"],
+                    "fetched_result": fetched,
+                    "time": str(fetch_time),
+                }
+                for fetched, stop in zip(res, c.kvg_stop_mapping["stops"])
+            ]
+        )
+
+        json.dump(
+            res_dict,
+            open("/opt/data_collection/output.json", "w"),
+            ensure_ascii=False,
+        )
         print(end="", flush=True)
         sleep(c.FECTH_DELAY)
 
