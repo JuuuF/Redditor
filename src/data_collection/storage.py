4312ce723d25b181331c51cdef4e6ce6f91fc1e4
Justin <juswerth@gmx.de>
2026-03-30 23:02:21 +0200

    Initialize minio connection for raw data storage

--- src/data_collection/storage.py
+++ src/data_collection/storage.py
@@ -1,5 +1,29 @@
+# User module imports
+import constants as c
+
 # Python module imports
 import json
+from minio import Minio
+
+# ------------------------------------------------------------------------
+# Initialization
+
+# Initialize Client
+client = Minio(
+    endpoint="minio:9000",
+    access_key=c.MINIO_ROOT_USER,
+    secret_key=c.MINIO_ROOT_PASSWORD,
+    secure=False,
+)
+
+
+# Initialize bucket if not existing
+buckets = [b.name for b in client.list_buckets()]
+if not c.MINIO_BUCKET_RAW in buckets:
+    client.make_bucket(c.MINIO_BUCKET_RAW)
+    print(f"Created MinIO bucket {c.MINIO_BUCKET_RAW} for raw API data.", flush=True)
+
+# ------------------------------------------------------------------------
 
 
 # Store data away
