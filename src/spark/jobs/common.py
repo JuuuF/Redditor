dfdf70d292c80a2c61566c38c3095c6fb334d1da
Justin <juswerth@gmx.de>
2026-04-15 17:47:18 +0200

    Create file for common spark job functionality

---
+++ src/spark/jobs/common.py
@@ -0,0 +1,30 @@
+"""
+File for common spark job functionality.
+"""
+
+from pyspark.sql import SparkSession
+
+# ------------------------------------------------------------------------
+# General
+
+
+def create_spark_session() -> SparkSession:
+    builder = SparkSession.builder.appName("MinIO Connectivity Test")
+
+    # Init MinIO connectivity
+    #fmt: off
+    builder = builder \
+        .config("spark.hadoop.fs.s3a.endpoint", "http://minio:9000") \
+        .config("spark.hadoop.fs.s3a.access.key", "minioadmin") \
+        .config("spark.hadoop.fs.s3a.secret.key", "minioadmin") \
+        .config("spark.hadoop.fs.s3a.path.style.access", "true") \
+        .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
+        .config("spark.hadoop.fs.s3a.aws.credentials.provider", "org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider")
+    
+    builder = builder \
+        .config("spark.sql.adaptive.enabled", "true") \
+        .config("spark.sql.adaptive.coalescencePartitions.enabled", "true")
+    # fmt: on
+
+    spark = builder.getOrCreate()
+    return spark
